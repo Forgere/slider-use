fac7107776449c08e3b8e7ed955c7c2fe0cd29b8
forgere <573596314@qq.com>
2016-04-19 18:10:10 +0800

    做slider

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,13 @@ class PagesController < ApplicationController
     raise ActiveRecord::RecordNotFound , "Couldn't find page with slug=#{params[:id]}" if @page.blank?
     render template_for(@page)
   end
+
+  def index
+    @microposts = Page.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @microposts }
+    end
+  end
 end
