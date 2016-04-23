1ddc4e9824728577b5eb4fea2f87d742447f0827
johnable <johnable@gmail.com>
2016-04-23 12:57:51 +0800

    修正了模型设置

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,8 +3,8 @@ class Upload < ActiveRecord::Base
   mount_uploader :img, ImgUploader
   attr_accessible :name, :mobile, :img, :upload_type, :city, :is_contact, :original_filename, :vote_id, :vote_count
   attr_accessor :captcha
-  validates :name, format: { with: /\A[a-zA-Z0-9]{3,10}\z/,
-    message: "3~10个字符，支持中文或英文，不含空格及特殊符号" }
+  validates :name, format: { with: /\A[a-zA-Z0-9]{1,100}\z/,
+    message: "2个字符以上，支持中文或英文，不含空格及特殊符号" }
   validates :mobile, format:{ with: /\A[0-9]{11}\z/,
     message: "必须填写正确的手机号码"
   }
