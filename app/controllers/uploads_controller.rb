7b248473a34da56dd6ad5c7e568bcb9e0c1da03a
johnable <johnable@gmail.com>
2016-04-24 19:23:41 +0800

    修正了参数返回

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -31,7 +31,7 @@ class UploadsController < ApplicationController
                 upload.save
                 render text: '点赞成功'
               else
-                render text: "#{@upload.img.url}:#{@upload.upload_type}"
+                render text: "#{@upload.img.url};#{@upload.upload_type}"
               end
             }
           else
